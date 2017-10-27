5f6c1a584179fbc65848be29cfe6185de08095e9
Tomaspereyra <31147365+Tomaspereyra@users.noreply.github.com>
2017-10-27 17:18:32 -0300

    Agregando el .exe y un instalador dentro de la carpeta "dist"

---
+++ SpaceInvaders-master/setup.py
@@ -0,0 +1,14 @@
+import os
+import cx_Freeze
+
+executables = [cx_Freeze.Executable("spaceinvaders.py")]
+os.environ['TCL_LIBRARY'] = r'C:\Users\Tomas\AppData\Local\Programs\Python\Python36-32\tcl\tcl8.6'
+os.environ['TK_LIBRARY'] = r'C:\Users\Tomas\AppData\Local\Programs\Python\Python36-32\tcl\tk8.6'
+cx_Freeze.setup(
+    name="Anillos de Saturno",
+    options={"build_exe": {"packages":["pygame"],
+             "include_files":["spaceinvaders.py","assets/a1_0.png","assets/a1_1.png","assets/a2_0.png",
+                              "assets/a2_1.png","assets/a3_0.png","assets/a3_1.png","shooter.png","fondonuevo.jpeg","controles.jpeg","618.jpg","star.jpg"]}},
+    executables = executables
+
+    )
